6f4df156503d46962e3e2c3a6d34c45ae5eaea68
flakboy <kacper1609@wp.pl>
2024-05-26 19:10:45 +0200

    implemented handle server-side events

--- IO-phone/src/app/register/register.component.ts
+++ IO-phone/src/app/register/register.component.ts
@@ -3,9 +3,12 @@ import { Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { LobbyEvent, LobbyEventStatus } from './model';
+import { ChangeDetectorRef } from '@angular/core';
 
 import {MatButton} from "@angular/material/button";
+
 import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { RegisterService } from './register.service';
 import { IStudent } from './model';
@@ -16,7 +19,7 @@ import { IStudent } from './model';
   imports: [
     NgIf,
     FormsModule,
-    MatButton, MatInputModule
+    MatButton, MatFormFieldModule, MatInputModule
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
@@ -27,13 +30,17 @@ export class RegisterComponent {
     name: ""
   };
 
-  constructor(private router: Router, private registerService: RegisterService) {}
+  constructor(
+    private router: Router,
+    private changeDetector: ChangeDetectorRef,
+    private registerService: RegisterService
+  ) {
+  }
 
   ngOnInit() {
-    //FIXME: subscription should be canceled when on unmount/redirection! 
-
-    this.registerService.status.subscribe(event => {
-      switch (event.status) {
+    this.registerService.status.subscribe(msg => {
+      console.log("subscribe triggered");
+      switch (msg.status) {
         case LobbyEventStatus.SUCCESS:
           this.isRegistered = true;
           break;
@@ -54,7 +61,9 @@ export class RegisterComponent {
           this.displayErrorMesage("Wystąpił nieznany błąd.");
           break;
       }
-    })
+      this.changeDetector.detectChanges();  // hack (?) allowing to detect changes of this.isRegistered and update the component;
+                                            // for some reason the view doesn't update without it  
+    });
   }
 
   ngOnDestroy() {
@@ -62,6 +71,8 @@ export class RegisterComponent {
   }
 
   submitForm() {
+    
+
     this.registerService.register(this.model.name);
   }
 
--- IO-phone/src/app/register/register.service.ts
+++ IO-phone/src/app/register/register.service.ts
@@ -3,6 +3,8 @@ import { Subject } from 'rxjs';
 import { LobbyEvent, LobbyEventStatus } from './model';
 
 const SERVER_URL = "http://localhost:8000" 
+const SUCCESS_MSG = "registered successfully";
+
 
 @Injectable({
   providedIn: 'root'
@@ -11,38 +13,50 @@ export class RegisterService {
 
   public readonly status: Subject<LobbyEvent> = new Subject();
 
-  // public sse: EventSource;
+  public sse: EventSource | null = null;
 
   constructor() { }
+
+  ngOnDestroy() {
+    this.sse?.close();
+  }
 
   register(username: string): void {
     //TODO: try to register to SSE, and await info about session start/user being kicked out;
 
-    fetch(`${SERVER_URL}/register`, {
-      method: "POST",
-      mode: 'cors',
-      headers: new Headers({"content-type": "application/json"}),
-      body: JSON.stringify({
-        "name": username
-      })
-    })
-    .then(res => {
-      switch (res.status) {
-        case 200:
-          this.status.next({status: LobbyEventStatus.SUCCESS});
+    // try {
+      this.sse = new EventSource(
+        `${SERVER_URL}/register/${username}`,
+        { withCredentials: true, }        //makes it possible to connect with server with different origin
+      );
 
-          // this.sse =
-          // new EventSource(`${SERVER_URL}/`);
-          break;
-        case 409:   //HTTP 409 Conflict - User already exists
-          this.status.next({status: LobbyEventStatus.DUPLICATE_USER});
-          break;
-        default:
-          this.status.next({status: LobbyEventStatus.UNKNOWN_ERROR});
-          break;
-      }
-    }).catch(err => {
+      // this.sse.onopen = e => {
+        // console.log("open", e);
+        // this.status.next({status: LobbyEventStatus.SUCCESS});
+      // }
+  
+      this.sse.onerror = (e) => {
+        console.log("error:", e);
+        //EventSource API doesn't allow to get status code of error, so we are probably forced to stick with generic error message 
         this.status.next({status: LobbyEventStatus.FAILED});
-    });
+        this.sse?.close();
+      }
+  
+      this.sse.onmessage = e => {
+        console.log("Message: ", e);
+        if (e.data === SUCCESS_MSG) {
+          this.status.next({status: LobbyEventStatus.SUCCESS});
+        } else {
+          console.log("Unknown message: ", e);
+        }
+        //TODO: handle different events
+      }
+    // } catch {
+      // if (this.sse?.OPEN) {
+      //   this.sse.close();
+      // }
+      // console.log("error!!!")
+      // this.status.next({status: LobbyEventStatus.FAILED});
+    // }
   }
 }
