3a9edf09574414f647c72528cdc462784abd5c63
Nepommuck <pawel.konopka5@gmail.com>
2024-06-09 11:12:30 +0200

    Get all polls REST API call

--- IO-pc-local/src/app/results/poll-results.service.ts
+++ IO-pc-local/src/app/results/poll-results.service.ts
@@ -1,120 +1,28 @@
 import {Injectable} from '@angular/core';
 import {PollResults} from "./model";
+import {API_URL} from "../../config";
+
+
+interface GetAllPollsApiResponse {
+  readonly results: PollResults,
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PollResultsService {
   get pollResults(): Promise<PollResults> {
-    // TODO SCRUM-68 Replace this mock implementation with the one based on REST API call
-    return Promise.resolve(mockPollResults)
+    return fetch(`${API_URL}/game/polls/all`, {
+      method: "get"
+    }).then(response => {
+      if (response.ok) {
+        return (response.json() as Promise<GetAllPollsApiResponse>)
+          .then(apiResponse => apiResponse.results)
+      } else {
+        return response.text().then(text => {
+          throw new Error(`Request for poll results failed: ${text}`);
+        });
+      }
+    })
   }
 }
-
-const mockPollResults: PollResults = [
-  {
-    personName: "Maciej",
-    questions: [
-      {
-        question: "Czy jest okej?",
-        answers: [
-          {
-            respondentName: "Igor",
-            answer:
-              "Jeszcze jak!",
-          },
-          {
-            respondentName: "Konrad",
-            answer:
-              "No nie bardzo",
-          },
-        ],
-      },
-      {
-        question: "Czy lubi dżem?",
-        answers: [
-          {
-            respondentName: "Igor",
-            answer:
-              "Ciężko stwierdzić",
-          },
-          {
-            respondentName: "Konrad",
-            answer:
-              "Na pewno tak!",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    personName: "Igor",
-    questions: [
-      {
-        question: "Czy jest okej?",
-        answers: [
-          {
-            respondentName: "Konrad",
-            answer:
-              "Konrad fajny jest",
-          },
-          {
-            respondentName: "Maciej",
-            answer:
-              "Konrad dziwny jest",
-          },
-        ],
-      },
-      {
-        question: "Czy lubi dżem?",
-        answers: [
-          {
-            respondentName: "Konrad",
-            answer:
-              "Dżem dżem dżem",
-          },
-          {
-            respondentName: "Maciej",
-            answer:
-              "Dżem",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    personName: "Konrad",
-    questions: [
-      {
-        question: "Czy jest okej?",
-        answers: [
-          {
-            respondentName: "Igor",
-            answer:
-              "Zemsta zemsta zemsta na wroga",
-          },
-          {
-            respondentName: "Maciej",
-            answer:
-              "Z Bogiem lub choćby mimo Boga",
-          },
-        ],
-      },
-      {
-        question: "Czy lubi dżem?",
-        answers: [
-          {
-            respondentName: "Igor",
-            answer:
-              "Konrad lubi dżem.",
-          },
-          {
-            respondentName: "Maciej",
-            answer:
-              "Konrad kocha dżem.",
-          },
-        ],
-      },
-    ],
-  },
-]
