0a73821ec6399a8d835461baad88046a9afbe66c
Kacper Mazurczyk <58552499+flakboy@users.noreply.github.com>
2024-06-11 20:40:29 +0200

    [SCRUM-80 + SCRUM-89] - widok wypełniania ankiety z mockowych danych + przesyłanie wypełnionego arkusza na serwer (#13)

---
+++ IO-phone/src/app/poll/model.ts
@@ -0,0 +1,58 @@
+/* eslint-disable camelcase */
+/* eslint-disable @typescript-eslint/camelcase */
+
+export enum QuestionType {
+    TEXTBOX = 1,
+    MULTIPLE_CHOICE,
+    SINGLE_CHOICE
+}
+
+export interface QuestionOption{
+    readonly text: string;
+    readonly isSelected: boolean;
+}
+
+
+//thise interface is probably invalid and/or unnecessary 
+export interface QuestionTextbox { 
+    readonly isVisible: boolean;
+    readonly initialValue: string;
+    readonly value: string;
+}
+
+//thise interface is probably invalid and/or unnecessary
+export interface Question {
+    readonly type: QuestionType,
+    readonly text: string;
+}
+
+//thise interface is probably invalid and/or unnecessary
+export interface Answer {
+    readonly question_name: string;       //should I use camelCase to keep JS conventions, or snake_case for Python backend?
+    // readonly value: QuestionTextbox | QuestionOption[];
+    readonly answer: string | string[];
+}
+
+
+export interface User {
+    name: string
+}
+export interface PollAnswer {
+    readonly answers: Answer[];
+    readonly user_about: User;
+    readonly user_filling: User;
+}
+
+export interface QuestionMap {
+    readonly [key: string]: Question;
+}
+
+export interface Poll {
+    readonly title: string;
+    readonly description: string;
+    readonly questions: QuestionMap;
+}
+
+export interface Student {
+    name: string;
+}
--- IO-phone/src/app/poll/poll.component.ts
+++ IO-phone/src/app/poll/poll.component.ts
@@ -1,20 +1,109 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { PollService } from './poll.service';
+import { Answer, Poll, Question, Student } from './model';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-poll',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule, ReactiveFormsModule,
+    MatButtonModule, MatFormFieldModule, MatInputModule, MatInputModule
+  ],
   templateUrl: './poll.component.html',
   styleUrl: './poll.component.css'
 })
-export class PollComponent implements OnInit {
+export class PollComponent {
   private pollService = inject(PollService);
+  private formBuilder = inject(FormBuilder);
+  private router = inject(Router);
 
-  ngOnInit(): void {
-    //TODO SCRUM-82: fetch a poll template from server and store it
-    // this.pollService.getPollTemplate(); 
+  pollTemplate: Poll | null = null;
+  questions: Question[] = [];
+
+  students: Student[] = [];
+  currentStudentIndex = -1;
+
+  pollForm: FormGroup = this.formBuilder.group({});
+
+  constructor() {
+    this.pollService.students$.subscribe(students => {
+      this.students = students;
+      this.currentStudentIndex = 0;
+    })
+    
+    this.pollService.pollTemplate$.subscribe(template => {
+      let questions = [];
+
+      // questions must be mapped to array, or the their original order won't be preserved because of the keyvalue pipe 
+      for (let [_key, value] of Object.entries(template.questions)) {
+        questions.push(value);
+
+        this.pollForm.addControl(value.text, this.formBuilder.control('', [Validators.required]));
+      }
+
+      this.pollTemplate = template;
+
+      this.questions = questions;
+
+    });
+
+    
+    this.pollService.fetchInfo();
   }
-  
 
+
+
+  submitForm() {
+    if (this.pollForm.valid) {
+      let answers = [];
+      
+      for (let question of this.questions) {
+        let questionName = question.text;
+
+        answers.push({
+          question_name: questionName,
+          answer: this.pollForm.get(questionName)!.value
+        });
+      }
+
+
+      this.pollService.saveAnswers({
+        answers: answers,
+        "user_about": {
+          "name": "marek"
+        },
+        "user_filling": {
+          "name": "jarek"
+        }
+      }).then(() => {
+        if (this.currentStudentIndex < this.students.length - 1) {
+          this.currentStudentIndex++;
+          this.clearForm();
+        } else {
+          alert("Wypelniono wszystkie ankiety!");
+          
+          //TODO: redirect to responses
+          this.router.navigate(["answers", this.pollService.username]);
+        }
+      })
+    } else {
+      //perhaps SnackBar would be better than alert
+      alert("Wartości w arkuszu są niepoprawne! Sprawdź czy udzielono odpowiedzi na wszystkie pytania.");
+    }
+
+  }
+
+  clearForm() {
+      this.pollForm.reset();
+  }
+
+  get username(): string {
+    return this.username;
+  }
 }
